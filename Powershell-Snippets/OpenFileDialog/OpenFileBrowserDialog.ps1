bb81d4811f64ab654a11aea0e02e82b6e85ae26b
Kasper Svendsen <52733424+KasperSvendsenGit@users.noreply.github.com>
2021-01-26 10:44:58 +0100

    Add files

---
+++ Powershell-Snippets/OpenFileDialog/OpenFileBrowserDialog.ps1
@@ -0,0 +1,6 @@
+﻿# Opens a simple file browser dialog with the default folder 'Desktop', to select a file and load it into $FileBrowser.
+
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+	InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+}
+$null = $FileBrowser.ShowDialog()
